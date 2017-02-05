7c35ac862c122864afc7512d77c1feca9a8007b8
Tomislav Stanković <tomislavstankovic1@gmail.com>
2017-02-05 17:41:56 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,24 +2,60 @@ var express = require('express');  // poziva express iz node_modules
 var app = express();  // označava da naša aplikacija koristi express
 var mysql = require('mysql');  // poziva mysql iz node_modules
 var bodyParser = require('body-parser');  // poziva bodyParser iz node_modules
+var multer  = require('multer'); // poziva multer iz node_modules
+var upload = multer({ dest: './uploads' }); //definira mapu u kojoj će se nalaziti uploadane datoteke
+var fs = require('fs'); // poziva fs iz node_modules
+var gcm = require('node-gcm'); // poziva gcm iz node_modules
+var gcmApiKey = 'AAAAUEKeGco:APA91bFbgLqCdxN019scRTL0Gw8CqqVETUfNj5eHyc7queYz-ldFh1QT8E4--Du7wPCdXF2n5Q9qTQyjaFZj2Ic7AUwukRqN-P7DSXlkBFA39gKAJIxZ-WLMnqlfD5ZIgOfIWBPaQ-X7';
 
 // označava da app koristi bodyParser() kako bi mogao dobiti podatke iz POST zahtjeva
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
- // definira port na kojemu će API raditi
-var port = process.env.PORT || 8080;       
+var port = process.env.PORT || 8080;        // definira port na kojemu će API raditi
 
 // Spajanje s MySQL bazom
  var pool = mysql.createPool({
         host: 'localhost',
-        user: '',
+        user: 'root',
         password: '',
-        database: ''
+        database: 'korisniciapi'
     }); 
 
 // definiranje zadane (defaultne) rute za naš API
 var apiRoutes = express.Router();
+
+//Spremanje DEVICE_ID za push notifikacije
+apiRoutes.post('/device-token', function (req, res, next) {
+
+   //console.log(req.body);
+   
+   var token = req.body.token;
+   console.log(token);
+
+   pool.getConnection(function(err, connection) {
+ 
+        if (err) {
+            console.error("Dogodila se greška: " + err);
+        }
+            
+			var uredjaj = { 
+            dev_token: token  
+            };   
+
+            connection.query('INSERT INTO device SET ?', uredjaj,
+            function(err, rows) {
+                if (err) {
+                    throw err;
+                } else {
+                    var result = {
+                        success: true,
+                        detail: rows
+                    }
+                }
+            });
+    }); 
+})
 
 // middleware
 apiRoutes.use(function(req, res, next) {
@@ -30,12 +66,14 @@ apiRoutes.use(function(req, res, next) {
 
 // testiranje rute (GET http://localhost:8080/api)
 apiRoutes.get('/', function(req, res) {
-    //ako je sve ispravno postavljeno kao odgovor ćemo dobiti ovu poruku
-    res.json({ message: 'API radi!' });   
+    res.json({ message: 'API radi!' });   //ako je sve ispravno postavljeno kao odgovor ćemo dobiti ovu poruku
 });
 
 //Dodavanje korisnika
-apiRoutes.post('/dodajkorisnika', function (req, res, next) {
+apiRoutes.post('/dodajkorisnika', upload.any(),function (req, res, next) {
+
+  console.log(req.body);
+  console.log(req.files);
 
    pool.getConnection(function(err, connection) {
  
@@ -45,15 +83,59 @@ apiRoutes.post('/dodajkorisnika', function (req, res, next) {
 
             var korisnik = { 
             k_ime: req.body.ime,
-            k_prezime: req.body.prezime
+            k_prezime: req.body.prezime,
+			k_slika: req.files[0].destination + "/" + req.files[0].filename,  
+			k_dokument: req.files[1].destination + "/" + req.files[1].filename  
             };      
+			
+        //SELECT svih tokena i slanje push notifikacije
+        pool.getConnection(function(err, connection) {
+ 
+        if (err) {
+            console.error("Dogodila se greška: " + err);
+        }
+       var query = "SELECT * FROM device ORDER BY iddevice ASC";
+        query = mysql.format(query);
+        connection.query(query,function(err,rows){
+           connection.release();
+            if (err) {               
+               return next(err);
+            } else {
+                 var SviTokeni = [];
+                 rows.forEach(function(a) {
+                        var jedanToken = {
+                             dev_token: a.dev_token
+                            }
+                            SviTokeni.push(jedanToken.dev_token);
+                        });
+            //Push
+            var device_tokens = SviTokeni; // array jedinstvenih tokena uređaja
+    var retry_times = 4; // broj puta koliko će se push notifikacija pokušati poslati ako ne uspije prvi put
+    var sender = new gcm.Sender(gcmApiKey); // pošiljatelj
+    var message = new gcm.Message(); // nova poruka
+    message.addData('title', 'Dodan novi korisnik:');
+    message.addData('message', korisnik.k_ime + ' ' + korisnik.k_prezime);
+    message.addData('sound', 'default');
+    message.collapseKey = 'Testing Push'; // grupa push notifikacija
+    message.delayWhileIdle = true; //odgoditi slanje push notifikacije ako je uređaj offline
+    message.timeToLive = 3; //broj sekundi koliko push notifikaciju držati na serveru ako je uređaj offline
+    sender.send(message, device_tokens[0], retry_times, function (result) {
+        console.log('Push poslan na: ' + device_tokens);
+      //  res.status(200).send('Push notifikacija poslana na uređaj: ' + device_tokens);
+    }, function (err) {
+      //  res.status(500).send('Neuspješno poslana push notifikacija');
+    });
+            }
+        });  
+        
+    })
 			
             connection.query('INSERT INTO korisnik SET ?', korisnik,
             function(err, rows) {
                 if (err) {
                     throw err;
                 } else {
-	            res.json("Uspješno dodan korisnik!");
+					res.json("Uspješno dodan korisnik!");
                     res.end();
                 }
                 connection.release();
@@ -72,7 +154,7 @@ pool.getConnection(function(err, connection) {
 
        var query = "SELECT * FROM korisnik ORDER BY k_id ASC";
        
-       var table = ["korisniciapi"];
+        var table = ["korisniciapi"];
         
         query = mysql.format(query,table);
 
@@ -98,7 +180,7 @@ pool.getConnection(function(err, connection) {
         if (err) {
             console.error("Dogodila se greška: " + err);
         }
-	
+
            var korisnik = { 
             k_ime: req.body.ime,
             k_prezime: req.body.prezime
@@ -124,20 +206,25 @@ pool.getConnection(function(err, connection) {
 });
 
 //Brisanje korisnika
-apiRoutes.delete('/korisnik/:k_id', function(req, res, next){
+apiRoutes.delete('/korisnik/:k_id/:k_slika/:k_dokument', function(req, res, next){
+
+console.log(req.params);
+
+var slika =  req.params.k_slika;
+var dokument =  req.params.k_dokument;
 
 pool.getConnection(function(err, connection) {
  
         if (err) {
             console.error("Dogodila se greška: " + err);
         }
-
             connection.query('delete from korisnik where k_id = ?', [req.params.k_id], 
-
             function(err, rows) {
                 if (err) {
                     return next(err);
                 } else {
+				    fs.unlinkSync('./uploads/' + slika);
+					fs.unlinkSync('./uploads/' + dokument);
                     res.writeHead(200, {
                         "Content-Type": "application/json"
                     });
@@ -150,6 +237,8 @@ pool.getConnection(function(err, connection) {
             });
     });
 });
+
+// ostale GET, POST, PUT, DELETE biti će definirane ovdje
 
 // sve rute sadržavati će /api
 app.use('/api', apiRoutes);
